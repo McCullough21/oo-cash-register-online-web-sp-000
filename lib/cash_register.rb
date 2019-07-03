59e009f55704170762c90ffcd171e2fef7cbf89d
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-03 20:03:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,11 @@
 class CashRegister
-attr_accessor :total, :discount, :items
+attr_accessor :total, :discount,
 
 
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @items = []
   end
 
 
